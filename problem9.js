9f9babe76bab273a34f554d57797f60b9b52f49c
Jordao Luveri <jordaoluveri@gmail.com>
2015-06-25 00:06:55 -0300

    Problem 9

---
+++ problem9.js
@@ -0,0 +1,24 @@
+//Problem 9
+
+//A Pythagorean triplet is a set of three natural numbers, a < b < c, for which, a2 + b2 = c2
+//For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+//There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+//Find the product abc.
+
+var get_p = function(){
+    for (var a = 1; a < 1000; a++){
+        for (var b = 1; b < 1000; b++){
+            for (var c = 334; c < 1000; c++){
+                if (a*a + b*b === c*c){
+                    if (a+b+c === 1000){
+                        return [a,b,c, a+b+c, a*b*c];
+                    }
+                }
+            }
+        }
+    }
+    
+};
+
+get_p();
